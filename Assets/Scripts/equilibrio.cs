021bcb377bc31f459aa6a3cae7502f7a75755005
azash666 <ddelgadogomis@hotmail.com>
2018-05-10 21:51:56 +0200

    10-05-2018

---
+++ Assets/Scripts/equilibrio.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class equilibrio : MonoBehaviour {
+	public GameObject DelanteIzquierda, DelanteDerecha, DetrasIzquierda, DetrasDerecha, Techo;
+	public bool AI, AD, DI, DD, Arriba;
+	public float fuerza;
+	private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		AI = DelanteIzquierda.GetComponent<detectaRueda> ().toca;
+		AD = DelanteDerecha.GetComponent<detectaRueda> ().toca;
+		DI = DetrasIzquierda.GetComponent<detectaRueda> ().toca;
+		DD = DetrasDerecha.GetComponent<detectaRueda> ().toca;
+		Arriba = Techo.GetComponent<detectaRueda> ().toca;
+		if (!DI && !AI && (DD || AD)) {
+			rb.AddRelativeTorque (0, 0, 30f);
+			//transform.Rotate (0, 0, -300 * Time.deltaTime);
+		}
+		if (!AD && !DD && DI || AI && !AD && !DD || !AD && !AI && !DD && !DI && Arriba) {
+			rb.AddRelativeTorque(0, 0, -30f);
+			//transform.Rotate (0, 0, 300 * Time.deltaTime);
+		}
+		if (!AD && !AI && (DI || DD)) {
+			rb.AddRelativeTorque(30f, 0, 0);
+			//transform.Rotate (-300 * Time.deltaTime, 0, 0);
+		}
+		if (!DD && !DI && (AI || AD)) {
+			rb.AddRelativeTorque(-30f, 0, 0);
+			//transform.Rotate (300 * Time.deltaTime, 0, 0);
+		}
+	}
+}
---
+++ Assets/Scripts/movimiento.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class movimiento : MonoBehaviour
+{
+	Rigidbody rb;
+	/*public GameObject camara;
+	public GameObject huecoCamara;
+	public GameObject torreta;
+	public Vector3 vel;*/
+
+	//[SyncVar]
+	private float anterior;
+	//[SyncVar]
+	private float tiempo;
+	//[SyncVar]
+	private float tiempoAnterior;
+	private float tiempodisparo;
+
+
+	//public GameObject huecoBala;
+	private float fuerza;
+	//public float fuerzaAngular;
+	//[SyncVar]
+	private float rotacionVertical;
+
+	private float velocidadAngular;
+
+	//[SyncVar]
+	//public float rotacion;
+
+
+
+
+	void Start()
+	{
+		//Cursor.visible = false;
+		fuerza = 5000f;
+		/*if(isLocalPlayer)
+			Instantiate (camara, huecoCamara.transform.position, huecoCamara.transform.rotation).transform.parent=huecoCamara.transform;*/
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate()
+	{
+		//rb.AddForce (0, -6f, 0);
+		/*if (!isLocalPlayer) {
+			if (!isServer) {
+				velocidadAngular = (rotacion - anterior) / (tiempo-tiempoAnterior);
+			}
+			rotacion += velocidadAngular * Time.deltaTime;
+			torreta.transform.localRotation = Quaternion.Euler (0, rotacion, 0);
+			float auxy = huecoCamara.transform.localRotation.eulerAngles.y;
+			huecoBala.transform.localRotation = Quaternion.Euler (rotacionVertical, auxy, 0);
+		}
+		else {*/
+			var x = Input.GetAxis ("Horizontal") * Time.deltaTime *60f;
+			var z = Input.GetAxis ("Vertical") *Time.deltaTime * fuerza/2;
+			//var rotacionTorreta = Input.GetAxis ("Mouse X") * Time.deltaTime * 60f;
+			//float aux = -Input.GetAxis ("Mouse Y") * Time.deltaTime * 50f;
+			/*float aux2 = huecoCamara.transform.localRotation.eulerAngles.x;
+			float auxy = huecoCamara.transform.localRotation.eulerAngles.y;
+			if (aux2 < 340f && aux2 >= 180f) {
+				rotacionVertical = 0;
+				huecoCamara.transform.localRotation = Quaternion.Euler (340f, auxy, 0);
+				huecoBala.transform.localRotation = Quaternion.Euler (340f, auxy, 0);
+			} else {
+				if (aux2 > 20f && aux2<180f) {
+					rotacionVertical = 0;
+					huecoCamara.transform.localRotation = Quaternion.Euler (20f, auxy, 0);
+					huecoBala.transform.localRotation = Quaternion.Euler (20f, auxy, 0);
+				} else {
+					rotacionVertical = aux;
+					if ((aux2 < 345f && aux2 > 180f && aux > 0) || (aux2 > 15f && aux2 < 180f && aux < 0) || aux2 >= 345f || aux2 <= 15f) {
+						huecoCamara.transform.Rotate (aux, 0, 0);
+						huecoBala.transform.Rotate (aux, 0, 0);
+					}
+				}
+			}*/
+			//float inclinacionBalas = huecoBala.transform.localRotation.eulerAngles.x;
+			//CmdCambiarVertical (inclinacionBalas);
+			//rb.AddTorque (0, x, 0);
+			transform.Rotate (0, x, 0);
+			//torreta.transform.Rotate (0, rotacionTorreta, 0);
+			//rotacion = torreta.transform.localRotation.eulerAngles.y;
+			//CmdCambiar(rotacion);
+			//CmdVelocidad (rb.velocity);
+		//	if (rb.velocity.magnitude <= 10)
+				//rb.AddForce (transform.forward * z);
+				transform.Translate(0,0,z*Time.deltaTime);
+			
+		//}
+	}
+
+
+	/*
+	[Command]
+	void CmdCambiar(float rot){
+		anterior = rotacion;
+		rotacion = rot;
+		tiempoAnterior = tiempo;
+		tiempo = Time.time;
+		velocidadAngular = (rotacion - anterior) / tiempo;
+	}
+
+	[Command]
+	void CmdCambiarVertical(float vert){
+		rotacionVertical = vert;
+	}
+*/
+}
